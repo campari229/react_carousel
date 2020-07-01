b228094b6eee3c1d0a08ae860a0b1a9ca6286fbe
Campari229 <KennyKoin@gmail.com>
2020-07-01 18:15:08 +0300

    finitie

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+import { CarouselList } from './CarouselList/CarouselList';
+import { Buttons } from './Buttons/Buttons';
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+import './Carousel.css';
 
-export default Carousel;
+export class Carousel extends React.Component {
+  state = {
+    imgPosition: 0,
+    step: 3,
+    animationDuration: 1000,
+    listItemWidth: 130,
+  }
+
+  moveRight = () => {
+    const numberOfItems = this.props.images.length;
+    const position = this.state.imgPosition;
+    const width = this.state.listItemWidth;
+    const steps = this.state.step;
+
+    if (position
+      <= -((numberOfItems - steps - (numberOfItems % steps)) * width)) {
+      this.setState(({
+        imgPosition:
+        -(width * (numberOfItems - steps)),
+      }));
+    } else {
+      this.setState(prevState => ({
+        imgPosition: prevState.imgPosition - (steps * width),
+      }));
+    }
+  }
+
+  moveLeft = () => {
+    const position = this.state.imgPosition;
+    const width = this.state.listItemWidth;
+    const steps = this.state.step;
+
+    if (position
+      >= -(steps * width)) {
+      this.setState(({
+        imgPosition: 0,
+      }));
+    } else {
+      this.setState(prevState => ({
+        imgPosition: prevState.imgPosition + (steps * width),
+      }));
+    }
+  }
+
+  render() {
+    const { images } = this.props;
+    const transformStyle = {
+      transform: `translateX(${this.state.imgPosition}px)`,
+      transition: `transform ${this.state.animationDuration}ms`,
+    };
+
+    return (
+      <div
+        className="Carousel"
+        style={
+          { width: `${(this.state.listItemWidth * this.state.step) + 30}px` }
+        }
+      >
+        <CarouselList images={images} transform={transformStyle} />
+        <Buttons moveLeft={this.moveLeft} moveRight={this.moveRight} />
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+};
